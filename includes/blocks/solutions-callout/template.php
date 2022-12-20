6dbfef6a2b6f877bfa4cbc2f2451df1e0060a6d6
Robert-at-486987442298 <robert@k4media.com>
2022-12-20 23:51:56 +0700

    Solutions callout

--- includes/blocks/solutions-callout/template.php
+++ includes/blocks/solutions-callout/template.php
@@ -1,26 +1,42 @@
 <?php
 
-     $height = "536";
-     $title  = "";
-     $image  = "";
-     $class  = "";
+     $title     = "";
+     $subtitle  = "";
+     $solutions = array();
 
-     // get fields via acf
-     if ( function_exists('get_field') ) {
+     // get fields
+     if ( function_exists('get_fields') ) {
           $title = get_field('title');
-          $image = get_field('image');
-     }
-     if ( is_front_page() ) {
-          $height = "844";
-          $class  = "front-page";
+          $subtitle = get_field('subtitle');
      }
 
-     // validate fields
-     if ( "" === $title ) {
-          $title = $post->post_title;
+     // get solutions pages
+     $page      = get_page_by_path( 'solutions' );
+     $args = array(
+          'post_type'      => 'page',
+          'posts_per_page' => -1,
+          'child_of'       => $page->ID,
+          'order'          => 'ASC',
+          'orderby'        => 'menu_order'
+     );
+     $pages = get_pages($args);
+     foreach( $pages as $p ) {
+          $solution = '<div class="solution ' . $p->post_title . ' ">';
+          $icon = get_stylesheet_directory() . '/assets/media/si-' . $p->post_name . '.svg';
+          $solution .= ( file_exists($icon) ) ? '<img src="' . get_stylesheet_directory_uri() . '/assets/media/si-' . $p->post_name . '.svg' . '">' : '<img src="' . get_stylesheet_directory_uri() . '/assets/media/si-missing.svg">' ;
+          $solution .= '<a href="' . get_permalink($p->ID) . '">'  ;
+          $solution .= $p->post_title;
+          $solution .= '</a>';
+          $solution .= '</div>';
+          $solutions[] = $solution;
      }
 
 ?>
-<div id="page-hero" class="<?php echo $class ?>" style="background-image:url(<?php if ( isset($image['url']) ) { echo $image['url']; } ?>)">
-     <div class="fp-hero-title"><h2><?php echo $title ?></h2></div>
+<div class="solutions-callout-stage">
+     <div class="solutions-callout silo">
+          <div class="title"><h2><?php echo $title ?></h2></div>
+          <div class="subtitle"><h3><?php echo $subtitle ?></h3></div>
+          <div class="solutions"><?php echo implode($solutions) ?></div>
+     </div>
+     <a class="button green ghost" href="<?php echo get_permalink(get_page_by_path( 'solutions' )) ?>">Learn More</a>
 </div>
