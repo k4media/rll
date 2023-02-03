7718689f06b8cf7f3322af16f7f18e53111fac94
Robert-at-486987442298 <robert@k4media.com>
2023-02-03 14:29:55 +0700

    Added link to awards callout placeholder

--- includes/blocks/awards-callout/template.php
+++ includes/blocks/awards-callout/template.php
@@ -15,5 +15,6 @@
 <div class="awards-callout-stage callout">
      <div class="awards-callout narrow">
           <h2>Awards Placeholder</h2>
+          <a href="<?php echo get_home_url(null, '/awards/') ?>">Awards Page</a>
      </div>
 </div>
