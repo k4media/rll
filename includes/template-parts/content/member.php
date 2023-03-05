0731d48b0a3babd4c77d708aedf35019f298366d
Robert-at-486987442298 <robert@k4media.com>
2023-03-05 18:49:02 +0700

    Added member rollover

--- includes/template-parts/content/member.php
+++ includes/template-parts/content/member.php
@@ -4,8 +4,12 @@
      * Template part for Member
      */
     $member_slug = sanitize_title($user->data->display_name);
-    $link        = get_home_url(null, 'teams/members/' . $member_slug . '/' . $user->data->ID . '/');
+    $link        = get_author_posts_url($user->data->ID);
+
+    /** Position */
     $position    = get_user_meta( $user->data->ID, 'position', true);
+
+    /** Locations */
     $locations   = array();
     $country_ids = get_user_meta( $user->data->ID, '_dfdl_user_country');
     foreach( $country_ids as $c ) {
@@ -13,10 +17,26 @@
         $locations[] = $country->name;
     }
 
+    /** Country expertise */
+    $country_expertise = array();
+    $countries = get_user_meta( $user->data->ID, '_dfdl_user_country_expertise');
+    foreach( $countries as $c ) {
+        $country = get_term( $c, 'dfdl_countries', true);
+        $country_expertise[] = $country->name;
+    }
+
+    /** Solution Services */
+    $expertise   = array();
+    $solution_ids = get_user_meta( $user->data->ID, '_dfdl_user_solutions');
+    foreach( $solution_ids as $s ) {
+        $solution = get_term( $s, 'dfdl_solutions', true);
+        $expertise[] = $solution->name;
+    }
+
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class("member-loop"); ?>>
-    <div class="team-member">
-        <a href="<?php echo $link ?>">
+<a href="<?php echo $link ?>">
+    <article id="post-<?php the_ID(); ?>" <?php post_class("member-loop"); ?>>
+        <div class="team-member">
             <img src="<?php echo get_avatar_url($user->data->ID, array('size' => 320)) ?>">
             <div class="details-stage">
                 <div class="details">
@@ -29,6 +49,34 @@
                     <?php endif; ?>
                 </div>
             </div>
-        </a>
-    </div>
-</article><!-- #post-<?php the_ID(); ?> -->
+        </div>
+        <div class="rollover">
+            <div class="stage">
+                <?php if ( isset($position) ) : ?>
+                    <h4>Position</h4>
+                    <div class="ranking"><?php echo $position ?></div>
+                <?php endif; ?>
+                <?php if ( isset($expertise) && count($expertise) > 0 ) : ?>
+                    <h4>Key Services</h4>
+                    <ul>
+                    <?php 
+                        foreach( $expertise as $e ) {
+                            echo '<li>' . $e . '</li>';
+                        }
+                    ?>
+                    </ul>
+                <?php endif; ?>
+                <?php if ( isset($country_expertise) && count($country_expertise) > 0 ) : ?>
+                    <h4>Key Countries</h4>
+                    <ul>
+                    <?php 
+                        foreach( $country_expertise as $c ) {
+                            echo '<li>' . $c . '</li>';
+                        }
+                    ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </div>
+    </article><!-- #post-<?php the_ID(); ?> -->
+</a>
