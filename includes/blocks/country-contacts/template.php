b7dee03b3628d8abcf5f4573209bbf9985accbd8
Robert-at-486987442298 <robert@k4media.com>
2023-04-09 17:22:33 +0700

    Added Google map link to country contact block

--- includes/blocks/country-contacts/template.php
+++ includes/blocks/country-contacts/template.php
@@ -1,22 +1,33 @@
 <?php
 
-     $address = "";
-     $contact = "";
-     $hours   = "";
+     
+     $address   = "";
+     $contact   = "";
+     $hours     = "";
+     $countries = "";
 
      // get fields
      if ( function_exists('get_fields') ) {
-          $address = get_field('address');
-          $contact = get_field('contact');
-          $hours   = get_field('hours');
+          $address   = get_field('address');
+          $contact   = get_field('contact');
+          $hours     = get_field('hours');
+          $countries = get_field('countries', 'options');
      }
-
+     
+     $section = dfdl_get_section();
+     if ( is_array($section) ) {
+          $map = $countries[end($section)]['map'];
+     }
+     
 ?>
 <div class="country-contacts-stage callout">
      <div class="country-contacts silo">
           <div class="address">
                <h3>Address</h3>
                <?php echo nl2br($address) ?>
+               <?php if ( isset($map) ) : ?>
+                    <br><a href='<?php echo $map ?>'>View on Google Maps</a>
+               <?php endif; ?>
           </div>
           <div class="contact">
                <h3>Contact</h3>
