824790fc8e3cad1060b1655f775cb7e43c83bff0
Robert-at-486987442298 <robert@k4media.com>
2023-04-11 14:29:33 +0700

    updated newsletter sigup target

--- includes/template-parts/footer/newsletter-signup.php
+++ includes/template-parts/footer/newsletter-signup.php
@@ -1,4 +1,4 @@
 <div>
     <label><?php  _e('Sign up for our Newsletter', 'dfdl') ?></label>
-    <a class="button subscribe" href="https://dfdl.us2.list-manage.com/subscribe?u=93756f829bfe5a9c6f2b4b655&id=f87f27907b">Subscribe</a>
+    <a target="_blank" class="button subscribe" href="https://dfdl.us2.list-manage.com/subscribe?u=93756f829bfe5a9c6f2b4b655&id=f87f27907b">Subscribe</a>
 </div>
