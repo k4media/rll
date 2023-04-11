2eea80391d1138a012d82117844cddabcee2d3b2
Robert-at-486987442298 <robert@k4media.com>
2023-04-11 20:44:15 +0700

    remove member rollover trans delay

--- includes/blocks/insights-callout/template.php
+++ includes/blocks/insights-callout/template.php
@@ -42,8 +42,12 @@ if( isset($maybe_country) && in_array( $maybe_country, constant('DFDL_COUNTRIES'
 ?>
 <div class="insights-callout-stage callout">
      <div class="insights-callout silo">
-          <h2><?php echo $title ?></h2>
-          <h3><?php echo $subtitle ?></h3>
+          <?php if ( isset($title) && $title !== "") : ?>
+               <h2><?php echo $title ?></h2>
+          <?php endif; ?>
+          <?php if ( isset($subtitle) && $subtitle !== "") : ?>
+               <h3><?php echo $subtitle ?></h3>
+          <?php endif; ?>
           <div class="posts <?php echo $post_class ?>" >
                <?php if ( $insights->have_posts() ) : ?>
                     <?php while ( $insights->have_posts() ) : ?>
@@ -53,30 +57,11 @@ if( isset($maybe_country) && in_array( $maybe_country, constant('DFDL_COUNTRIES'
 
                          // setup the post
                          $insights->the_post();
-
                          $term = dfdl_post_solution($post->ID);
 
                          // pass data to template
                          set_query_var("story", $post );
                          set_query_var("term", $term);
-
-                         
-
-                         //set_query_var("term", $category);
-                         //set_query_var("slug", dfdl_content_hub_category($post->ID));
-                         //$post_terms = wp_get_post_terms($post->ID, 'category');
-                         //var_dump($post_terms);
-                         //var_dump(dfdl_content_hub_category($post->ID));
-                         /*
-                         $startdate = get_post_meta( $post->ID, 'startdate', true);
-                         if ( isset($startdate) ) {
-                         $show_date = mysql2date( get_option( 'date_format' ), $startdate );
-                         }
-                         set_query_var("sponsor", get_post_meta( $post->ID, 'sponsor', true));
-                         set_query_var("dateline", get_post_meta( $post->ID, 'dateline', true));
-                         set_query_var("timeline", get_post_meta( $post->ID, 'timeline', true));
-                         set_query_var("show_date", $show_date);
-                         */
 
                          // define template
                          $file = get_stylesheet_directory() . '/includes/template-parts/content/insights-' . $term->slug . '-card.php';
@@ -93,8 +78,6 @@ if( isset($maybe_country) && in_array( $maybe_country, constant('DFDL_COUNTRIES'
                     <?php get_template_part( 'includes/template-parts/content/content-none' ); ?>
                <?php endif; ?>
           </div>
-
           <div class="jump"><a class="button green ghost see-all" href="<?php echo $jump ?>">See More</a></div>
-          
      </div>
 </div>
