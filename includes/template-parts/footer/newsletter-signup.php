6ec61c8252cf888776d03bb4552b1a558be76a1f
Robert-at-486987442298 <robert@k4media.com>
2023-04-11 14:28:59 +0700

    Update: change newsletter  signup to button

--- includes/template-parts/footer/newsletter-signup.php
+++ includes/template-parts/footer/newsletter-signup.php
@@ -1,19 +1,4 @@
 <div>
-    <!-- Begin Mailchimp Signup Form -->
-    <form action="https://dfdl.us2.list-manage.com/subscribe/post?u=93756f829bfe5a9c6f2b4b655&amp;id=f87f27907b&amp;f_id=00c7c2e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <label><?php  _e('Sign up for our Newsletter', 'dfdl') ?></label>
-        <div class="newsletter-signup">
-            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
-            <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-            <input type="image" id="mc-embedded-subscribe" src="<?php echo get_stylesheet_directory_uri() ?>/assets/media/go-arrow.svg" name="subscribe" value="Subscribe">
-        </div>
-        <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display:none"></div>
-            <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_93756f829bfe5a9c6f2b4b655_f87f27907b" tabindex="-1" value=""></div>
-    </form>
-    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[5]='MMERGE5';ftypes[5]='text';fnames[6]='MMERGE6';ftypes[6]='text';fnames[7]='MMERGE7';ftypes[7]='text';fnames[8]='MMERGE8';ftypes[8]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-    <!--End mc_embed_signup-->
+    <label><?php  _e('Sign up for our Newsletter', 'dfdl') ?></label>
+    <a class="button subscribe" href="https://dfdl.us2.list-manage.com/subscribe?u=93756f829bfe5a9c6f2b4b655&id=f87f27907b">Subscribe</a>
 </div>
