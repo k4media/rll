ed2a7356a5699e648a5e5a78438691f52be593da
Robert-at-486987442298 <robert@k4media.com>
2023-04-17 15:44:12 +0700

    Country nav responsive

--- includes/blocks/country-navigation/template.php
+++ includes/blocks/country-navigation/template.php
@@ -15,17 +15,19 @@ foreach($countries as $id) {
           $nav[] = '<li><a href="#">' . $page_title . '</a></li>' ;
      } else {
           if ( in_array(strtolower($page_slug), $pieces)  ) {
-               $nav[] = '<li><a class="current-menu-item" href="' . get_permalink($id) . '">' . $page_title . '</a></li>' ;
+               $nav[] = '<li class="swiper-slide"><a class="current-menu-item" href="' . get_permalink($id) . '">' . $page_title . '</a></li>' ;
           } else {
-               $nav[] = '<li><a href="' . get_permalink($id) . '">' . $page_title . '</a></li>' ;
+               $nav[] = '<li class="swiper-slide"><a href="' . get_permalink($id) . '">' . $page_title . '</a></li>' ;
           }
      }
 
 }
 
-echo '<nav class="country-subnav-stage desks-subnav"><ul>';
+
+
+echo '<nav id="location-subnav" class="country-subnav-stage desks-subnav"><div class="subnav-swiper"><ul class="swiper-wrapper">';
 echo implode($nav);
-echo '</ul></nav>';
+echo '</ul></div></nav>';
 
 
 ?>
