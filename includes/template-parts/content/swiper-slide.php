806ef2e2b7c1018ece66524575af9bf22f60ebcf
Robert-at-486987442298 <robert@k4media.com>
2023-05-17 13:55:21 +0700

    Trim slider titles and excerpts

--- includes/template-parts/content/swiper-slide.php
+++ includes/template-parts/content/swiper-slide.php
@@ -2,8 +2,19 @@
     <div class="swiper-card">
         <div class="stage">
             <div class="taxonomy"><?php echo $categories?></div>
-            <h2><a href="<?php echo get_the_permalink($story->ID) ?>"><?php echo wp_trim_words($story->post_title, 12) ?></a></h2>
-            <div class="excerpt"><?php echo get_the_excerpt($story->ID) ?></div>
+            <h2>
+                <a href="<?php echo get_the_permalink($story->ID) ?>">
+                    <?php
+                        echo wp_trim_words($story->post_title, 12);
+                    ?>
+                </a>
+            </h2>
+            <div class="excerpt">
+                <?php
+                    // echo get_the_excerpt($story->ID);
+                    echo wp_trim_words(get_the_excerpt($story->ID), 28);
+                    ?>
+                </div>
             <a class="button green ghost" href="<?php echo get_the_permalink($story->ID) ?>">Read More</a>
         </div>
     </div>
