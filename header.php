4362973160e46e8e5d505efe1441eecd25659458
Robert-at-486987442298 <robert@k4media.com>
2023-08-14 20:53:29 +0700

    Add google site verification

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="google-site-verification" content="3QPW51nG0zbRexH1c-u_HYsIkZz4OvTwJmWgT9GERXg" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
